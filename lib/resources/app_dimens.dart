f4e8a5ef1d17fb64d56262a874f7b53a259e6b58
leeeeeoy <hoheho18@naver.com>
2022-03-06 17:23:09 +0900

    add: 텍스트 스타일 추가

--- lib/resources/app_dimens.dart
+++ lib/resources/app_dimens.dart
@@ -1,1 +1,6 @@
 part of 'resources.dart';
+
+abstract class AppDimens {
+  static const regular = FontWeight.w400;
+  static const bold = FontWeight.w700;
+}
--- lib/resources/app_styles.dart
+++ lib/resources/app_styles.dart
@@ -1,1 +1,59 @@
 part of 'resources.dart';
+
+abstract class AppStyles {
+  static const bold28 = TextStyle(
+    fontSize: 28,
+    fontWeight: AppDimens.bold,
+    color: AppColors.gray6,
+    letterSpacing: -0.28,
+    height: 84 / 28,
+  );
+
+  static const bold20 = TextStyle(
+    fontSize: 20,
+    fontWeight: AppDimens.bold,
+    color: AppColors.gray6,
+    letterSpacing: -0.2,
+    height: 28 / 20,
+  );
+
+  static const bold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: AppDimens.bold,
+    color: AppColors.gray6,
+    letterSpacing: -0.16,
+    height: 24 / 16,
+  );
+
+  static const regular16 = TextStyle(
+    fontSize: 16,
+    fontWeight: AppDimens.regular,
+    color: AppColors.gray6,
+    letterSpacing: -0.16,
+    height: 24 / 16,
+  );
+
+  static const regular14 = TextStyle(
+    fontSize: 14,
+    fontWeight: AppDimens.regular,
+    color: AppColors.gray6,
+    letterSpacing: -0.14,
+    height: 22 / 14,
+  );
+
+  static const bold12 = TextStyle(
+    fontSize: 12,
+    fontWeight: AppDimens.bold,
+    color: AppColors.gray6,
+    letterSpacing: -0.12,
+    height: 20 / 12,
+  );
+
+  static const regular12 = TextStyle(
+    fontSize: 12,
+    fontWeight: AppDimens.regular,
+    color: AppColors.gray6,
+    letterSpacing: -0.12,
+    height: 20 / 12,
+  );
+}
